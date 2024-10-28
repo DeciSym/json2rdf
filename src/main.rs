58a80d161b8140ec2817b113ffa8277f35a3bb2f
bharath181 <bharathselvaraj18@gmail.com>
2024-10-28 16:56:48 -0400

    Created working JSON reader to parse code and generate RDF. Currently everything is a string, need to make it into oxrdf graph

--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,103 @@
-use oxrdf::NamedOrBlankNode;
-use serde_json::Value;
+use oxrdf::{Graph, NamedOrBlankNode};
+use serde::de::value;
+use serde_json::{Deserializer, Value};
 use std::collections::{HashMap, VecDeque};
 use std::fs::File;
 use std::io::BufReader;
-use struson::reader::*;
 
 fn main() {
-    // build using struson
-    //-------------------------
-    let mut json_reader = JsonStreamReader::new(
-        r#"{
-        "aircraft": {
-            "id": "A12345",
-            "model": "Boeing 747",
-            "manufacturer": "Boeing",
-            "capacity": {
-                "seats": 416,
-                "cargo_volume": 150.4
+    let file = File::open("src/airplane.json").unwrap();
+    let reader = BufReader::new(file);
+    let stream = Deserializer::from_reader(reader).into_iter::<Value>();
+
+    let mut subject_stack: VecDeque<String> = VecDeque::new();
+    let mut array_properties: HashMap<String, String> = HashMap::new();
+    let mut property: Option<String> = None;
+
+    for value in stream {
+        match value {
+            Ok(Value::Object(obj)) => {
+                let subject = format!("_:b{}", subject_stack.len()); // Create a new blank node
+                println!("Created new subject: {}", subject);
+                subject_stack.push_back(subject.clone());
+
+                if let Some(last_subject) = subject_stack.back() {
+                    println!("The last subject in the stack is: {}", last_subject);
+                    if let Some(prop) = &property {
+                        println!("Adding property for parent -> child relationship: {}", prop);
+                    }
+                }
+
+                for (key, val) in obj {
+                    property = Some(format!("#{}", key));
+                    println!("Processing key: {}", key);
+                    process_value(&mut subject_stack, &property, val);
+                }
+
+                // End of object; remove the subject from stack
+                subject_stack.pop_back();
             }
-        }
-    }"#
-        .as_bytes(),
-    );
-
-    let mut subject_stack: VecDeque<NamedOrBlankNode> = VecDeque::new();
-    let mut array_properties: HashMap<NamedOrBlankNode, NamedOrBlankNode> = HashMap::new();
-
-    json_reader.begin_object().unwrap();
-    json_reader.next_name().unwrap();
-
-    // println!("hi");
-
-    // json_reader.begin_array().unwrap();
-    // assert_eq!(json_reader.next_number::<u32>().unwrap().unwrap(), 1);
-    // assert_eq!(json_reader.next_bool().unwrap(), true);
-    // json_reader.end_array().unwrap();
-
-    json_reader.begin_object().unwrap();
-
-    while json_reader.has_next().unwrap() {
-        match json_reader.peek().unwrap() {
-            ValueType::Array => match token {
-                if token 
-            },
-            ValueType::Object => todo!(),
-            ValueType::String => println!("A string: {}", json_reader.next_string().unwrap()),
-            ValueType::Boolean => println!("a bool: {}", json_reader.next_bool().unwrap()),
-            ValueType::Number => println!(
-                "A string: {}",
-                json_reader.next_number::<u32>().unwrap().unwrap()
-            ),
-
-            _ => panic!("Unexpected type"),
-        }
-    }
-
-    json_reader.end_object().unwrap();
-    // Ensures that there is no trailing data
-    json_reader.consume_trailing_whitespace().unwrap();
-    //-------------------------
-
-    //built using serde_json
-    //-------------------------
-    // let file = File::open("src/airplane.json").unwrap();
-    // let reader = BufReader::new(file);
-
-    // // Parse the JSON file into a serde_json::Value
-    // let json_data: Value = serde_json::from_reader(reader).unwrap();
-    // parse_json(&json_data);
-    //-------------------------
-}
-
-fn parse_json(value: &Value) {
-    match value {
-        Value::Null => println!("null"),
-        Value::Bool(b) => println!("Boolean: {}", b),
-        Value::Number(n) => println!("Number: {}", n),
-        Value::String(s) => println!("String: {}", s),
-        Value::Array(arr) => {
-            println!("Array:");
-            for (index, item) in arr.iter().enumerate() {
-                print!("Index {}: ", index);
-                parse_json(item);
+            Ok(Value::Array(arr)) => {
+                if let Some(last_subject) = subject_stack.back() {
+                    if let Some(prop) = &property {
+                        array_properties.insert(last_subject.clone(), prop.clone());
+                    }
+                }
+                for val in arr {
+                    process_value(&mut subject_stack, &property, val);
+                }
             }
-        }
-        Value::Object(obj) => {
-            println!("Object:");
-            for (key, val) in obj.iter() {
-                print!("Key: {}, Value: ", key);
-                parse_json(val);
+            Ok(other) => {
+                process_value(&mut subject_stack, &property, other);
+            }
+            Err(e) => {
+                eprintln!("Error parsing JSON: {}", e);
             }
         }
     }
 }
 
-
-// psuedocode
-// subject array
-// properties array 
-
-// property = null
-
-// While Next val exists
-
-//     if start of array 
-        
-//     if end of array
-
-//     if start of object
-//         create blank node
-//         if property exists and subject isnt empty create triples with subject array.last(),property,subject)
-//         add blank node to subject array to track
-//     if end of object
-//         .pop() on subject array
-//         WHAT DOES THIS MEAN( if (!subjectStack.isEmpty() && arrayProperties.containsKey(subjectStack.getLast())) property = arrayProperties.get(subjectStack.getLast());)
-//     if string
-//         create triple with last element of blank node array, property , string val
-//     if number
-//         create triple with last element of blank node array, property , number val
-//     if bool
-//         create triple with last element of blank node array, property , bool val
-//     if key_name
-//         property = key_name
+fn process_value(subject_stack: &mut VecDeque<String>, property: &Option<String>, value: Value) {
+    if let Some(last_subject) = subject_stack.back() {
+        println!("The last subject in the stack is: {}", last_subject);
+        if let Some(prop) = property {
+            println!("Processing property: {}", prop);
+            match value {
+                Value::Bool(b) => {
+                    println!("Boolean value: {}", b);
+                }
+                Value::Number(num) => {
+                    let literal = if let Some(int) = num.as_i64() {
+                        int.to_string()
+                    } else if let Some(float) = num.as_f64() {
+                        float.to_string()
+                    } else {
+                        return;
+                    };
+                    println!("Number value: {}", literal);
+                }
+                Value::String(s) => {
+                    println!("String value: {}", s);
+                }
+                Value::Null => {
+                    println!("Null value");
+                }
+                Value::Object(obj) => {
+                    let subject = format!("_:b{}", subject_stack.len());
+                    println!("Created nested subject: {}", subject);
+                    subject_stack.push_back(subject);
+                    for (key, val) in obj {
+                        let nested_property = Some(format!("#{}", key));
+                        process_value(subject_stack, &nested_property, val);
+                    }
+                    subject_stack.pop_back();
+                }
+                Value::Array(arr) => {
+                    for val in arr {
+                        process_value(subject_stack, property, val);
+                    }
+                }
+            }
+        }
+    }
+}
