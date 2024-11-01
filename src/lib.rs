aa161881f6ba0e555912e18d0f416a061257716a
bharath181 <bharathselvaraj18@gmail.com>
2024-11-01 14:34:02 -0400

    Added additional documentation for Rust Docs. Returns either a graph or a file with triples

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+//! # JSON2RDF Converter Library
+//!
+//! This library provides functionality for converting JSON data into RDF format.
+//! It uses `serde_json` for JSON parsing and `oxrdf` to build and manage RDF graphs.
+//!
+//! ## Overview
+//! - Converts JSON data structures into RDF triples, generating a graph representation.
+//! - Supports blank nodes for nested structures and maps JSON properties to RDF predicates.
+//!
+//! ## Features
+//! - Handles JSON Objects, Arrays, Booleans, Numbers, and Strings as RDF triples.
+//! - Allows specifying a custom RDF namespace for generated predicates and objects.
+//! - Outputs the RDF data to a specified file or prints it to the console.
+
 use oxrdf::vocab::xsd;
 use oxrdf::{BlankNode, Graph, Literal, NamedNodeRef, TripleRef};
 
@@ -6,7 +20,34 @@ use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{BufReader, Write};
 
-pub fn json_to_rdf(file_path: &String, namespace: &Option<String>, output_file: &Option<String>) {
+pub enum GraphOrMessage {
+    Graph(Graph),
+    Message(String),
+}
+
+/// Converts JSON data to RDF format.
+///
+/// This function reads JSON data from the specified file, processes it into RDF triples,
+/// and outputs the RDF graph. Users can specify a namespace to use for RDF predicates and
+/// an output file for saving the generated RDF data.
+///
+/// # Arguments
+/// - `file_path`: Path to the JSON file.
+/// - `namespace`: Optional custom namespace for RDF predicates.
+/// - `output_file`: Optional output file path for writing RDF data.
+///
+/// # Example
+/// ```rust
+/// use json2rdf::json_to_rdf;
+///
+/// json_to_rdf(&"test/airplane.json".to_string(), &Some("http://example.com/ns#".to_string()), &Some("output.nt".to_string()));
+/// ```
+
+pub fn json_to_rdf(
+    file_path: &String,
+    namespace: &Option<String>,
+    output_file: &Option<String>,
+) -> Result<GraphOrMessage, String> {
     let rdf_namespace: String = if namespace.is_some() {
         namespace.clone().unwrap()
     } else {
@@ -67,12 +108,43 @@ pub fn json_to_rdf(file_path: &String, namespace: &Option<String>, output_file: 
         }
     }
 
-    let mut buffer: Vec<u8> = Vec::new();
-    writeln!(buffer, "{}", graph.to_string()).unwrap();
-
-    let file = File::create("foo.txt");
-    writeln!(file.unwrap(), "{}", String::from_utf8_lossy(&buffer)).unwrap();
+    if let Some(output_path) = output_file {
+        let mut file = File::create(output_path).expect("Error creating file");
+        writeln!(file, "{}", graph).expect("Error writing to file");
+        Ok(GraphOrMessage::Message(format!(
+            "RDF created at: {}",
+            output_path
+        )))
+    } else {
+        Ok(GraphOrMessage::Graph(graph))
+    }
 }
+
+/// This function handles different JSON data types, converting each into RDF triples:
+/// - JSON Objects create new blank nodes and recursively process nested values.
+/// - JSON Arrays iterate over each element and process it as an individual value.
+/// - JSON Booleans, Numbers, and Strings are converted to RDF literals.
+///
+/// # Recursion for Nested Structures
+/// Recursion is used to handle deeply nested JSON structures, which may contain multiple
+/// levels of objects or arrays. This recursive approach allows the function to "dive" into
+/// each nested layer of a JSON structure, creating blank nodes for sub-objects and handling
+/// them as new subjects within the RDF graph. As a result, each level of JSON data is
+/// systematically transformed into RDF triples, regardless of complexity or depth.
+///
+/// # Arguments
+/// - `subject_stack`: Stack of blank nodes representing subjects. Each nested level pushes a new subject to the stack.
+/// - `property`: RDF predicate (property) associated with the JSON value.
+/// - `value`: JSON value to process.
+/// - `graph`: RDF graph where triples are added.
+/// - `namespace`: Namespace for generating predicate URIs.
+///
+/// # JSON Type to RDF Conversion
+/// - **Object**: Creates a blank node and recursively processes key-value pairs.
+/// - **Array**: Iterates over elements and processes each as a separate value.
+/// - **String**: Converts to `xsd:string` literal.
+/// - **Boolean**: Converts to `xsd:boolean` literal.
+/// - **Number**: Converts to `xsd:int` or `xsd:float` literal based on value type.
 
 fn process_value(
     subject_stack: &mut VecDeque<BlankNode>,
