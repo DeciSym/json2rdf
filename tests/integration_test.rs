6a33d056bbf1dc7d814ee899436a61ba323fe8b1
Greg Hanson <greg.hanson@decisym.ai>
2025-02-20 12:42:37 -0500

    add test for writing to file

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,13 +1,41 @@
+// Copyright (c) 2024-2025, DeciSym, LLC
+// Licensed under either of:
+// - Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+// - BSD 3-Clause License (https://opensource.org/licenses/BSD-3-Clause)
+// at your option.
+
 use json2rdf::json_to_rdf;
+use oxrdfio::{RdfFormat, RdfParser};
+use std::fs::{self, File};
 
 #[test]
 fn test_graph_triple_count() {
     let triple_count_string = json_to_rdf(&"tests/airplane.json".to_string(), &None, &None);
 
-    match triple_count_string {
-        Ok(res) => {
-            assert_eq!(res.unwrap().len(), 23)
-        }
-        Err(e) => eprintln!("Error writing: {}", e),
-    }
+    assert!(triple_count_string.is_ok());
+    assert_eq!(triple_count_string.unwrap().unwrap().len(), 23);
+}
+
+#[test]
+fn test_graph_write() {
+    let output = "out.nt".to_string();
+    let _ = fs::remove_file(output.clone());
+
+    let res = json_to_rdf(
+        &"tests/airplane.json".to_string(),
+        &None,
+        &Some(output.clone()),
+    );
+
+    assert!(res.is_ok());
+    assert!(res.unwrap().is_none());
+
+    let f = File::open(output.clone()).expect("unable to open output file for result verification");
+    let quads = RdfParser::from_format(RdfFormat::NTriples)
+        .for_reader(f)
+        .collect::<Result<Vec<_>, _>>()
+        .expect("failed to parse generated output file");
+
+    assert_eq!(quads.len(), 23);
+    let _ = fs::remove_file(output.clone());
 }
